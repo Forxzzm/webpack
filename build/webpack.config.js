99b0232d8dfccacb396e47cac81d454d4be9389c
张志明 <14779809290@163.com>
2020-08-12 11:38:46 +0800

    feat： 多路口打包基础案例

---
+++ build/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')    
+module.exports = {
+    mode:'development', // 开发模式
+    entry: {            // 入口文件
+        main: path.resolve(__dirname,'../src/main.js'),
+        mainCopy: path.resolve(__dirname,'../src/main-copy.js'),
+    },    
+    output: {
+        filename: '[name].[hash:8].js',      // 打包后的文件名称
+        path: path.resolve(__dirname,'../dist')  // 打包后的目录
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+          template:path.resolve(__dirname,'../public/index.html'),   //打包到的地方
+          filename:'index.html',
+          chunks:['main'] // 与入口文件对应的模块名
+        }),
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,'../public/index-copy.html'),   //打包到的地方
+            filename:'index-copy.html',
+            chunks:['mainCopy'] // 与入口文件对应的模块名
+        })
+    ]
+}
