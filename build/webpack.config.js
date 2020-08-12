bea9223b4f0ef0f7d7a99da64371cf6aefe32af4
张志明 <14779809290@163.com>
2020-08-12 15:14:34 +0800

    chore： 去除多路口配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -8,12 +8,13 @@ const vueLoaderPlugin = require('vue-loader/lib/plugin')                //解析
 
 module.exports = {
     mode:'development', // 开发模式
-    entry: {            // 入口文件
-        main:["@babel/polyfill",path.resolve(__dirname,'../src/main.js')],
-        mainCopy:["@babel/polyfill",path.resolve(__dirname,'../src/main-copy.js')]
+    entry: ["@babel/polyfill",path.resolve(__dirname,'../src/main.js')],
+    // entry: {            // 入口文件
+        // main:["@babel/polyfill",path.resolve(__dirname,'../src/main.js')],
+        // mainCopy:["@babel/polyfill",path.resolve(__dirname,'../src/main-copy.js')]
         // main: path.resolve(__dirname,'../src/main.js'),
         // mainCopy: path.resolve(__dirname,'../src/main-copy.js'),
-    },    
+    // },    
     output: {
         filename: '[name].[hash:8].js',      // 打包后的文件名称
         path: path.resolve(__dirname,'../dist')  // 打包后的目录
@@ -30,11 +31,11 @@ module.exports = {
           filename:'index.html',
           chunks:['main'] // 与入口文件对应的模块名
         }),
-        new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,'../public/index-copy.html'),   //打包到的地方
-            filename:'index-copy.html',
-            chunks:['mainCopy'] // 与入口文件对应的模块名
-        }),
+        // new HtmlWebpackPlugin({
+        //     template:path.resolve(__dirname,'../public/index-copy.html'),   //打包到的地方
+        //     filename:'index-copy.html',
+        //     chunks:['mainCopy'] // 与入口文件对应的模块名
+        // }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "[name].[hash].css",
